f4b36024703662f7b50b204c7bf17b60d3c23253
rencell <rencelltobelonia67@gmail.com>
2024-10-02 00:07:35 +0800

    navigator top bar

--- Tobelonia/app/urls.py
+++ Tobelonia/app/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 
-from .views import index
+from . import views
 
 app_name = 'app'
 urlpatterns = [
-    path('', index.as_view(), name="app_index"),  
+    path('', views.index.as_view(), name="index"),  
+    path('settings/', views.Settings.as_view(), name="settings"),  
+    path('services/', views.Services.as_view(), name="services"),  
+    path('pricing/', views.Pricing.as_view(), name="pricing"),  
+    path('contacts/', views.Contact.as_view(), name="contacts"),  
 ]
--- Tobelonia/app/views.py
+++ Tobelonia/app/views.py
@@ -1,6 +1,31 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views.generic import TemplateView
 
-class index(TemplateView):
-    template_name = 'app/index.html'
+class Mixin:
+    def get_context_data(self, **kwargs):
+        
+        data = super().get_context_data(**kwargs)
+        
+        data['active_home'] = self.request.path == '/'
+        data['active_settings'] = self.request.path == '/settings/'
+        data['active_services'] = self.request.path == '/services/'
+        data['active_pricing'] = self.request.path == '/pricing/'
+        data['active_contacts'] = self.request.path == '/contacts/'
+        return data
 
+
+class index(Mixin, TemplateView):
+    template_name = 'app/Home.html'
+
+class Settings(Mixin, TemplateView):
+    template_name = 'app/Settings.html'
+    
+class Services(Mixin, TemplateView):
+    template_name = 'app/Services.html'
+
+class Pricing(Mixin, TemplateView):
+    template_name = 'app/Pricing.html'
+
+class Contact(Mixin, TemplateView):
+    template_name = 'app/Contact.html'
